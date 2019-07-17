efaf531605da0fa3dd482521efaf6546dd33af14
kolesnikov <shadarsi@gmail.com>
2019-07-17 14:51:42 +0300

    add structure

---
+++ src/services/swapi-service.js
@@ -0,0 +1,44 @@
+export default class SwapiService {
+
+    _apiBase = 'https://swapi.co/api';
+
+    async getResource(url) {
+    
+        const rel = await fetch(`${this._apiBase}${url}`);
+    
+        if (!rel.ok) {
+           throw new Error (`Could not fetch ${url}, received ${rel.status}`) 
+        }
+    
+        const body = await rel.json();
+        
+        return body;
+    }
+    getAllPeople() {
+        return this.getResource('/people/')
+    }
+
+    getAllPeopleId(id) {
+        return this.getResource(`/people/${id}/`)
+    }
+    getAllPlanet() {
+        return this.getResource('/planet/')
+    }
+
+    getAllPlanetId(id) {
+        return this.getResource(`/planet/${id}/`)
+    }
+    getAllStarships() {
+        return this.getResource('/starships/')
+    }
+
+    getAllStarshipId(id) {
+        return this.getResource(`/starships/${id}/`)
+    }
+};
+
+const swapi = new SwapiService();
+
+swapi.getAllStarships().then((body) => {
+    console.log(body)
+});
