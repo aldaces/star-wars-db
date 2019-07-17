3a2b2218ad319862622b141655e3fd085c97240b
kolesnikov <shadarsi@gmail.com>
2019-07-17 14:38:29 +0300

    add class service

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-const getResource = async (url) => {
+class SwapiService {
+
+    _apiBase = 'https://swapi.co/api';
+
+    async getResource(url) {
     
-    const rel = await fetch(url);
-    const body = await rel.json();
+        const rel = await fetch(`${this._apiBase}${url}`);
     
-    return body;
+        if (!rel.ok) {
+           throw new Error (`Could not fetch ${url}, received ${rel.status}`) 
+        }
+    
+        const body = await rel.json();
+        
+        return body;
+    }
+    getAllPeople() {
+        return this.getResource('/people/')
+    }
+
+    getAllPeopleId(id) {
+        return this.getResource(`/people/${id}/`)
+    }
+    getAllPlanet() {
+        return this.getResource('/planet/')
+    }
+
+    getAllPlanetId(id) {
+        return this.getResource(`/planet/${id}/`)
+    }
+    getAllStarships() {
+        return this.getResource('/starships/')
+    }
+
+    getAllStarshipId(id) {
+        return this.getResource(`/starships/${id}/`)
+    }
 };
 
-getResource('https://swapi.co/api/people/1/').then( (body) => {
-    console.log(body);
+const swapi = new SwapiService();
+
+swapi.getAllStarships().then((body) => {
+    console.log(body)
 });
